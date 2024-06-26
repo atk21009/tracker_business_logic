c424dc0ef0f874db36d8eedcf7cfe3edfae13cb1
atk21009 <tja21009@byui.edu>
2024-06-26 10:15:29 -0600

    Remove Cowboy

--- src/tracker_business_logic_app.erl
+++ src/tracker_business_logic_app.erl
@@ -10,21 +10,8 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    Dispatch = cowboy_router:compile([
-        {'_', [
-            %% Hello World
-            {"/", index_handler, []},
-            %% Package Info
-            {"/package/[...]", package_handler, []},
-            %% Location Update
-            {"/location/[...]", location_handler, []}
-        ]}
-    ]),
-    % URL http://localhost:8080
-    {ok,_} = cowboy:start_clear(
-        http,
-        [{port, 8080}],
-        #{env => #{dispatch => Dispatch}}),
+    request_sup:start_link(),
+    database_sup:start_link(),
     tracker_business_logic_sup:start_link().
 
 stop(_State) ->
